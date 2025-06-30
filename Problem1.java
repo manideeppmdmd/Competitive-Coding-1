8ffd677c703b9072e3f09b5346c37338060e1504
Manideep Pilli <manideep.pmd@gmail.com>
2025-06-30 00:37:14 -0600

    Added Interview problem

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,11 @@
+// Provided an array of n-1 integers in the range of 1 to n with no duplicates in list, One of the integers is missing in the list. Write a code to find the missing integer.
 
+// Examples:
+
+// Input : arr[] = [1, 2, 3, 5, 6, 7, 8]
+
+// Output : 4
+
+// Input : arr[] = [1, 2, 4, 5, 6, 7, 8, 9]
+
+// Output : 3
